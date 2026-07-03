cbc3f4b81cc13ff854a851091fd7333198c93828
czppeter <56679401+czppeter@users.noreply.github.com>
2026-07-03 22:27:34 +0800

    Modify comment for RNA removal in analysis script
    Updated comment to include snoRNA and snRNA in removal process.

--- script/Ribo-seq_analysis_pipline.sh
+++ script/Ribo-seq_analysis_pipline.sh
@@ -6,7 +6,7 @@ done
 
 
 #step2
-#remove rRNA/tRNA, download from:https://rnacentral.org/
+#remove rRNA/tRNA/snoRNA/snRNA, download from:https://rnacentral.org/
 #BSUB -n 12
 #BSUB -o rRNA.log
 source ~/env/samtools.env
